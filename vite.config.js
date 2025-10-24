3faf51e76ca4f243fdc98839fe471e3d68e9e45c
nunormoliveira <nunormoliveira@gmail.com>
2025-10-24 10:32:01 +0100

    vite update for forwarding local host via ngrok

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true,          // escuta em 0.0.0.0
+    port: 5173,
+    strictPort: true,
+    allowedHosts: true,  // permite hosts externos (ngrok)
+    cors: true
+    // Se precisares, podes limitar:
+    // allowedHosts: ['localhost', '.ngrok-free.app', '.ngrok-free.dev']
+  }
 })
